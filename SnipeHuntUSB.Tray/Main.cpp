141e692d520d4d8328aafbd75f6ca310404a2911
Steve <ciroque@live.com>
2025-03-13 20:51:07 -0700

    Move Resources to Resource Files

--- SnipeHuntUSB.Tray/Main.cpp
+++ SnipeHuntUSB.Tray/Main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include <shellapi.h>
-#include "../SnipeHuntUSB.Core/Core.h"
+#include "resource.h"
 
 #define WM_TRAY (WM_APP + 1)
 
@@ -8,26 +8,43 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     if (msg == WM_TRAY && lParam == WM_RBUTTONUP) {
         POINT pt;
         GetCursorPos(&pt);
-        HMENU hMenu = CreatePopupMenu();
-        AppendMenu(hMenu, MF_STRING, 1, L"Exit");
+        HMENU hMenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU));
+        HMENU hPopup = GetSubMenu(hMenu, 0);
         SetForegroundWindow(hwnd);
-        TrackPopupMenu(hMenu, TPM_RIGHTALIGN | TPM_BOTTOMALIGN, pt.x, pt.y, 0, hwnd, NULL);
+        TrackPopupMenu(hPopup, TPM_RIGHTALIGN | TPM_BOTTOMALIGN, pt.x, pt.y, 0, hwnd, NULL);
         DestroyMenu(hMenu);
     }
-    else if (msg == WM_COMMAND && LOWORD(wParam) == 1) {
-        PostQuitMessage(0);
+    else if (msg == WM_COMMAND) {
+        switch (LOWORD(wParam)) {
+        case IDM_SHOW_DEVICES:
+        case IDM_VIEW_LOGS:
+        {
+            wchar_t title[16], message[32];
+            LoadStringW(GetModuleHandle(NULL), IDS_501, title, sizeof(title) / sizeof(wchar_t));
+            LoadStringW(GetModuleHandle(NULL), IDS_NOT_IMPLEMENTED, message, sizeof(message) / sizeof(wchar_t));
+            MessageBoxW(hwnd, message, title, MB_ICONWARNING);
+        }
+        break;
+        case IDM_EXIT:
+            PostQuitMessage(0);
+            break;
+        }
     }
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+	LPCWSTR WINDOW_CLASS_NAME = L"SnipeHuntUSBTray";
+
     WNDCLASS wc = { 0 };
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = L"SnipeHuntUSBTray";
+    wc.lpszClassName = WINDOW_CLASS_NAME;
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindow(L"SnipeHuntUSBTray", L"", WS_OVERLAPPEDWINDOW,
+    wchar_t title[32];
+    LoadStringW(hInstance, IDS_WINDOW_TITLE, title, sizeof(title) / sizeof(wchar_t));
+    HWND hwnd = CreateWindow(WINDOW_CLASS_NAME, title, WS_OVERLAPPEDWINDOW,
         0, 0, 0, 0, NULL, NULL, hInstance, NULL);
 
     NOTIFYICONDATA nid = { sizeof(NOTIFYICONDATA) };
@@ -36,10 +53,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
     nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
     nid.uCallbackMessage = WM_TRAY;
     nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wcscpy_s(nid.szTip, L"SnipeHuntUSB");
+    LoadStringW(hInstance, IDS_TRAY_TOOLTIP, nid.szTip, sizeof(nid.szTip) / sizeof(wchar_t));
     Shell_NotifyIcon(NIM_ADD, &nid);
-
-    SayHello(); // Call DLL function
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
---
+++ SnipeHuntUSB.Tray/resource.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define IDM_SHOW_DEVICES    100
+#define IDM_VIEW_LOGS       101
+#define IDM_EXIT            102
+#define IDR_MENU            200
+
+#define IDS_NOT_IMPLEMENTED 300
+#define IDS_501             301
+#define IDS_WINDOW_TITLE    302
+#define IDS_TRAY_TOOLTIP    303
